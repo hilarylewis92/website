cb9917048af765691fe263a4572dd3a969536cc7
Hilary <hilarylewis92@gmail.com>
2021-02-18 14:28:43 -0700

    Update sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,23 +6,32 @@ export const Sidebar = () => {
   return (
     <div className="menu">
       <div className="icon-wrapper">
-        <AiFillMail className="icon" size={25} />
-        <AiFillGithub className="icon" size={25} />
-        <AiFillLinkedin className="icon" size={25} />
+        <a href="mailto:hilarylewis92@gmail.com" target="_blank">
+          <AiFillMail className="icon" size={25} />
+        </a>
+        <a href="https://github.com/hilarylewis92" target="_blank">
+          <AiFillGithub className="icon" size={25} />
+        </a>
+        <a href="https://www.linkedin.com/in/hilarylewis92/" target="_blank">
+          <AiFillLinkedin className="icon" size={25} />
+        </a>
       </div>
       <div className="menu-content">
         <a className="menu-link">
-          <h3 className="menu-title">Things I've built</h3>
+          <h3 className="menu-title">Portfolio</h3>
+          <h6 className="menu-subtitle">Things I've built</h6>
         </a>
         <hr />
 
         <a className="menu-link">
-          <h3 className="menu-title">My Experiences</h3>
+          <h3 className="menu-title">Resume</h3>
+          <h6 className="menu-subtitle">My Experiences</h6>
         </a>
         <hr />
 
         <a className="menu-link">
-          <h3 className="menu-title">Get my attention</h3>
+          <h3 className="menu-title">Contact</h3>
+          <h6 className="menu-subtitle">Get my attention</h6>
         </a>
       </div>
     </div>
